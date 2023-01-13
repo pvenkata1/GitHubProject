397fc2a8c643c48ecc75134c723e55fd1ae939e6
pvenkata1 <pvenkata@dotdashmdp.com>
2023-01-13 15:34:29 -0600

    animals calss

---
+++ src/test/java/org/example/Animals.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Animals {
+    //Enumerating objects: 22, done.
+    //Counting objects: 100% (22/22), done.
+    //Delta compression using up to 8 threads
+    //Compressing objects: 100% (12/12), done.
+}
